067910495161123e6aa9f33ace4ede438e2ecb70
Elias-Chairi <elcha002@osloskolen.no>
2022-04-04 08:54:58 +0200

    Initial Commit

---
+++ Kalkulator/KalkulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalkulatorApp.swift
+//  Kalkulator
+//
+//  Created by Elias Pettersen Chairi on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct KalkulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
